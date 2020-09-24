d675f8b44fc811c97e6212ace1a26de243bb929d
akivav <akiva.grobman@gmail.com>
2020-09-24 17:17:09 +0300

    implemented resizing

--- src/com/k300/graphics/Assets.java
+++ src/com/k300/graphics/Assets.java
@@ -1,5 +1,6 @@
 package com.k300.graphics;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Hashtable;
 import static com.k300.utils.Utils.loadImage;
@@ -17,11 +18,18 @@ public class Assets {
 
     private Assets() {
         images = new Hashtable<>();
-        BufferedImage smallRed = resizeImage(loadImage(RED_CAR_KEY, ".png"), loadImage(RED_CAR_KEY, ".png").getWidth() / 2, loadImage(RED_CAR_KEY, ".png").getHeight() / 2);
+        // these two variables represent the width relative to the height
+        final int widthWight = 3;
+        final int heightWight = 5;
+        // this will determine the size of the car
+        double multiplier = 1.5;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int carImageWidth = screenSize.width / (int)Math.floor(heightWight * multiplier);
+        int carImageHeight = screenSize.height / (int)Math.floor(widthWight * multiplier);
         images.put(TRACK_KEY, loadImage(TRACK_KEY, ".jpg"));
-        images.put(RED_CAR_KEY, smallRed);
-        images.put(BLUE_CAR_KEY, loadImage(BLUE_CAR_KEY, ".png"));
-        images.put(YELLOW_CAR_KEY, loadImage(YELLOW_CAR_KEY, ".png"));
+        images.put(RED_CAR_KEY, resizeImage(loadImage(RED_CAR_KEY, ".png"), carImageWidth, carImageHeight));
+        images.put(BLUE_CAR_KEY, resizeImage(loadImage(BLUE_CAR_KEY, ".png"), carImageWidth, carImageHeight));
+        images.put(YELLOW_CAR_KEY, resizeImage(loadImage(YELLOW_CAR_KEY, ".png"), carImageWidth, carImageHeight));
         images.put(PLAY_BUTTON_KEY, loadImage(PLAY_BUTTON_KEY, ".png"));
     }
 
