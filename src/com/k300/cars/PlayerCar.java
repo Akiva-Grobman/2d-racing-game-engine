0cdcc303d426057bde94a518b2ef50a493e247dd
ELK4N4 <elkana.hendler@gmail.com>
2020-10-05 15:55:26 +0300

    Fixing merge

--- src/com/k300/cars/PlayerCar.java
+++ src/com/k300/cars/PlayerCar.java
@@ -8,15 +8,19 @@ import java.awt.event.KeyListener;
 
 public class PlayerCar extends Car {
 
+    private final double CAR_TURNING_ANGLE;
+    private final int RIGHT_ROTATION;
+    private final int LEFT_ROTATION;
     private final PlayerKeyListener keyListener;
-    private final double carTurnAngle;
-    private Collisions collisions;
+    private final Collisions collisions;
 
 
     public PlayerCar(String carColor, Point startingPosition, Collisions collisions) {
         super(carColor, startingPosition);
         this.collisions = collisions;
-        carTurnAngle = 4;
+        CAR_TURNING_ANGLE = 4;
+        RIGHT_ROTATION = -1;
+        LEFT_ROTATION = 1;
         keyListener = new PlayerKeyListener();
     }
 
@@ -24,94 +28,115 @@ public class PlayerCar extends Car {
     @Override
     public void tick() {
         if(keyListener.getKeyIsPressed(PlayerKeyListener.UP_ARROW)) {
-            forward();
+            moveForward();
             if(isOffTrack()) {
-                backwards();
+                moveBackwards();
             }
         }
         if(keyListener.getKeyIsPressed(PlayerKeyListener.DOWN_ARROW)) {
-            backwards();
+            moveBackwards();
             if(isOffTrack()) {
-                forward();
+                moveForward();
             }
         }
         if(keyListener.getKeyIsPressed(PlayerKeyListener.RIGHT_ARROW)) {
-            right();
+            moveRight();
         }
         if(keyListener.getKeyIsPressed(PlayerKeyListener.LEFT_ARROW)) {
-            left();
+            moveLeft();
         }
     }
 
-    private void forward() {
+    private void moveForward() {
         double newX;
         double newY;
-        if(angle >= 0 && angle <= 90) {
-            newX = x + (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y - Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-        } else if(angle >= 90 && angle <= 180) {
-            newX = x - (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y + Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-        } else if(angle >= 180 && angle <= 270) {
-            newX = x - (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y + Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
+        if(isInBoundsOf(0, 90)) {
+            newX = x + xDistance();
+            newY = y - yDistance();
+        } else if(isInBoundsOf(90, 180)) {
+            newX = x - xDistance();
+            newY = y + yDistance();
+        } else if(isInBoundsOf(180, 270)) {
+            newX = x - xDistance();
+            newY = y + yDistance();
         } else /*if(angle >= 270 && angle <= 360)*/ {
-            newX = x + (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y - Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
+            newX = x + xDistance();
+            newY = y - yDistance();
         }
-        x = newX;
-        x = Math.round(x*10000)/10000.0; //5 numbers after the decimal point
-        y = newY;
-        y = Math.round(y*10000)/10000.0; //5 numbers after the decimal point
+        setNewXY(newX, newY);
     }
 
-    private void backwards() {
+    private void moveBackwards() {
         double newX;
         double newY;
-        if(angle >= 0 && angle <= 90) {
-            newX = x - (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y + Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-        } else if(angle >= 90 && angle <= 180) {
-            newX = x + (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y - Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-        } else if(angle >= 180 && angle <= 270) {
-            newX = x + (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y - Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
+        if(isInBoundsOf(0, 90)) {
+            newX = x - xDistance();
+            newY = y + yDistance();
+        } else if(isInBoundsOf(90, 180)) {
+            newX = x + xDistance();
+            newY = y - yDistance();
+        } else if(isInBoundsOf(180, 270)) {
+            newX = x + xDistance();
+            newY = y - yDistance();
         } else /*if(angle >= 270 && angle <= 360)*/ {
-            newX = x - (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
-            newY = y + Math.tan(Math.toRadians(angle)) * (speed / Math.sqrt(1 + Math.tan(Math.toRadians(angle)) * Math.tan(Math.toRadians(angle))));
+            newX = x - xDistance();
+            newY = y + yDistance();
         }
-        x = newX;
-        x = Math.round(x*10000)/10000.0; //5 numbers after the decimal point
-        y = newY;
-        y = Math.round(y*10000)/10000.0; //5 numbers after the decimal point
+        setNewXY(newX, newY);
     }
 
-    private void right() {
-        angle -= carTurnAngle;
-        if (angle == 90) {
-            angle -= carTurnAngle;
-        }
+    private boolean isInBoundsOf(int lowerBound, int upperBound) {
+        return angle >= lowerBound && angle <= upperBound;
+    }
 
+    // This is most of the distance formula (with the x extracted instead of the standard distance extracted).
+    // Meaning x +- distance() the -/+ will be determined by the angle and direction (in the forwards and backwards methods)
+    // and the y will use this as well for it's distance calculation.
+    private double xDistance() {
+        return (speed / sqrtOfSquareAnglePlusOne());
+    }
+
+    private double yDistance() {
+        return tanOfAngle() * xDistance();
+    }
+
+    private double sqrtOfSquareAnglePlusOne() {
+        return Math.sqrt(1 + tanOfAngle()*tanOfAngle());
+    }
+
+    private double tanOfAngle() {
+        return Math.tan(Math.toRadians(angle));
+    }
+
+    private void setNewXY(double newX, double newY) {
+        // rounding is for display prepossess
+        // 5 numbers after the decimal point
+        x = Math.round(newX * 10000) / 10000.0;
+        y = Math.round(newY * 10000) / 10000.0;
+    }
+
+    private void moveRight() {
+        turn(RIGHT_ROTATION);
+    }
+
+    private void moveLeft() {
+        turn(LEFT_ROTATION);
+    }
+
+    private void turn(int multiplier) {
+        angle += CAR_TURNING_ANGLE * multiplier;
+        if(angle == 90) {
+            angle += CAR_TURNING_ANGLE * multiplier;
+        }
+        resetAngle();
+    }
+
+    private void resetAngle() {
         if (angle > 360) {
             angle = 0;
         } else if (angle < 0) {
             angle = 360;
         }
-    }
-
-    private void left() {
-        angle += carTurnAngle;
-        if (angle == 90) {
-            angle += carTurnAngle;
-        }
-
-        if (angle > 360) {
-            angle = 0;
-        } else if (angle < 0) {
-            angle = 360;
-        }
-
     }
 
     private boolean isOffTrack() {
