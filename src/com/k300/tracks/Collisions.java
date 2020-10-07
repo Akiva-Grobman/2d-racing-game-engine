408e9e52fe2be51a307ca2077a60822a067e7236
akiva <akiva.grobman@gmail.com>
2020-10-07 22:40:04 +0300

    obstacles and track borders will be added to the track image immediately closing #36

--- src/com/k300/tracks/Collisions.java
+++ src/com/k300/tracks/Collisions.java
@@ -2,10 +2,6 @@ package com.k300.tracks;
 
 import com.k300.utils.Point;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class Collisions {
 
     private final Margins margins;
