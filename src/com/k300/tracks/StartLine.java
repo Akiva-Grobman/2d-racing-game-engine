a1afbc9e83fad546adcc08f66b2aac483ad6773a
ELK4N4 <elkana.hendler@gmail.com>
2020-10-13 14:20:30 +0300

    Added StartLine class

---
+++ src/com/k300/tracks/StartLine.java
@@ -0,0 +1,13 @@
+package com.k300.tracks;
+
+import com.k300.utils.Point;
+
+public class StartLine {
+    private Point upperStartLinePoint;
+    private Point lowerStartLinePoint;
+
+    public StartLine(Point upperStartLinePoint, Point lowerStartLinePoint) {
+        this.upperStartLinePoint = upperStartLinePoint;
+        this.lowerStartLinePoint = lowerStartLinePoint;
+    }
+}
