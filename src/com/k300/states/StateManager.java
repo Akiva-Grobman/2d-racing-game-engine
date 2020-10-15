673f18abe6658525af1fe54284bd7a149dcc33c2
akiva <akiva.grobman@gmail.com>
2020-10-15 03:51:24 +0300

    changed StateManager to implement the singleton style

--- src/com/k300/states/StateManager.java
+++ src/com/k300/states/StateManager.java
@@ -2,14 +2,36 @@ package com.k300.states;
 
 public class StateManager {
 
-    private static State currentState = null;
+
+    private static volatile StateManager singletonStateManagerInstance;
+    private State currentState;
+
+    private StateManager() {
+        currentState = null;
+    }
+
+    private static void handleInstance() {
+        if(!isInstantiated()) {
+            instantiate();
+        }
+    }
+
+    private static boolean isInstantiated() {
+        return singletonStateManagerInstance != null;
+    }
+
+    private static void instantiate() {
+        singletonStateManagerInstance = new StateManager();
+    }
 
     public static State getCurrentState() {
-        return currentState;
+        handleInstance();
+        return singletonStateManagerInstance.currentState;
     }
 
     public static void setCurrentState(State currentState) {
-        StateManager.currentState = currentState;
+        handleInstance();
+        singletonStateManagerInstance.currentState = currentState;
     }
 
 }
