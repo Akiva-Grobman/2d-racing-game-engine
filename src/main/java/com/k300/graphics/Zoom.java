666feb1297e6e755ccd0ba030e497ee843cdda27
ELK4N4 <elkana.hendler@gmail.com>
2020-10-26 12:32:43 +0200

    Cleaned useless function from Zoom class

--- src/main/java/com/k300/graphics/Zoom.java
+++ src/main/java/com/k300/graphics/Zoom.java
@@ -1,24 +1,9 @@
 package com.k300.graphics;
 
-import com.k300.utils.configarations.Config;
 import java.awt.image.BufferedImage;
 import static com.k300.utils.Utils.resizeImage;
 
 public class Zoom {
-
-//    public static BufferedImage getZoomedImage(double x, double y, BufferedImage image) {
-//        double widthFactorAfterConfig = image.getWidth() * Config.getZoomInFactor();
-//        double heightFactorAfterConfig = image.getHeight() * Config.getZoomInFactor();
-//        double statingZoomX = x - (widthFactorAfterConfig / 2);
-//        double startingZoomY = y - (heightFactorAfterConfig / 2);
-//        statingZoomX = clamp(statingZoomX, widthFactorAfterConfig, image.getWidth());
-//        startingZoomY = clamp(startingZoomY, heightFactorAfterConfig, image.getHeight());
-//        BufferedImage croppedImage = image.getSubimage((int) statingZoomX,
-//                (int) startingZoomY ,
-//                (int) widthFactorAfterConfig,
-//                (int) heightFactorAfterConfig);
-//        return resizeImage(croppedImage, croppedImage.getWidth(), croppedImage.getHeight());
-//    }
 
     public static BufferedImage getZoomedImage(double x, double y, double zoomFactor, BufferedImage image) {
         double zoomedWidth = image.getWidth() / zoomFactor;
