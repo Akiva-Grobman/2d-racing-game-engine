f932bd82973bec6b1eb399bf0565bc494cb014cc
akiva <akiva.grobman@gmail.com>
2020-10-26 20:30:10 +0200

    started documentation for PreferenceReader.java

--- src/main/java/com/k300/utils/configarations/PreferenceReader.java
+++ src/main/java/com/k300/utils/configarations/PreferenceReader.java
@@ -3,18 +3,41 @@ package com.k300.utils.configarations;
 import java.util.*;
 import java.util.prefs.Preferences;
 
+/*
+*   Purpose:
+*       This class object will represent the application database
+*   Contains:
+*       The preference object that is our database, and a HashMap that stores the date while the program is running.
+*       The Config class will read from the HashMap and write to both objects
+*   Values being stored:
+*       Zoom status: whether or not the user want to use the zoom in option in an online game
+*       Dev mode: used to flag parts that should only be called during development
+*       Server url: the url for the server duh
+*       Zoom factor: this is used to determine how much to zoom in when using the zoom in view
+*/
+
 public class PreferenceReader {
 
+    // This represent the string of the true boolean value
     private static final String TRUE = String.valueOf(true);
+    // zoom status key
     private static final String ZOOM_STATUS = "zoom";
+    // dev mode key
     private static final String DEV_MODE_STATUS = "devMode";
+    // server url key
     private static final String SERVER_URL = "url";
+    // zoom factor key
     private static final String ZOOM_FACTOR = "zoomFactor";
+    // the actual database
     private final Preferences preferences;
+    // the Hash map for run time storing
     private final Map<String, String> configData;
 
+    // only initialization option
     PreferenceReader() {
+        // bind to this object
         preferences = Preferences.userRoot().node(this.getClass().getName());
+        // initialize to previous preferences
         configData = getConfigAsMap();
     }
 
