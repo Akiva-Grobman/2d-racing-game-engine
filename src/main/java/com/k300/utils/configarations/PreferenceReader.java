313197f6c8da3985686781fab05aa801e58cde93
akiva <akiva.grobman@gmail.com>
2020-10-27 19:47:42 +0200

    finished documentation for PreferenceReader.java

--- src/main/java/com/k300/utils/configarations/PreferenceReader.java
+++ src/main/java/com/k300/utils/configarations/PreferenceReader.java
@@ -41,54 +41,77 @@ public class PreferenceReader {
         configData = getConfigAsMap();
     }
 
+    // This method will return the stored value in our database as a hashmap
     private Map<String, String> getConfigAsMap() {
+        //Initialize hashmap
         Map<String, String> map = new HashMap<>();
+        // This method follows this pattern
+        // 1. define default value
+        // 2. retrieve from database previous/default value
+        // set default
         String defaultZoomStatus = "false";
+        // retrieve data
         map.put(ZOOM_STATUS, preferences.get(ZOOM_STATUS, defaultZoomStatus));
+        // set default
         String defaultDevMode = "false";
+        // retrieve data
         map.put(DEV_MODE_STATUS, preferences.get(DEV_MODE_STATUS, defaultDevMode));
+        // set default
         String defaultUrl = "http://localhost:3000";
+        // retrieve data
         map.put(SERVER_URL, preferences.get(SERVER_URL, defaultUrl));
+        // set default
         String defaultZoomFactor = "3";
+        // retrieve data
         map.put(ZOOM_FACTOR, preferences.get(ZOOM_FACTOR, defaultZoomFactor));
         return map;
     }
 
+    // url accessor
     String getServerUrl() {
         return configData.get(SERVER_URL);
     }
 
+    // dev mode accessor
     boolean isInDevMode() {
         return configData.get(DEV_MODE_STATUS).contains(TRUE);
     }
 
+    // dev mode modifier
     void setIsInDevMode(boolean isInDevMode) {
         updateBoolean(DEV_MODE_STATUS, isInDevMode);
     }
 
+    // zoom mode accessor
     boolean isUsingZoom() {
         return configData.get(ZOOM_STATUS).contains(TRUE);
     }
 
-    void setIsUsingZoom(boolean isUsingZoom) {
-        updateBoolean(ZOOM_STATUS, isUsingZoom);
-    }
-
-    void setZoomInFactor(double zoomInFactor) {
-        updateString(ZOOM_FACTOR,
-                String.valueOf(zoomInFactor));
-    }
-
+    // zoom factor accessor
     double getZoomInFactor() {
         return Double.parseDouble(configData.get(ZOOM_FACTOR));
     }
 
+    // zoom mode modifier
+    void setZoomInFactor(double zoomInFactor) {
+        updateString(ZOOM_FACTOR, String.valueOf(zoomInFactor));
+    }
+
+    // zoom factor modifier
+    void setIsUsingZoom(boolean isUsingZoom) {
+        updateBoolean(ZOOM_STATUS, isUsingZoom);
+    }
+
+    // update database and hashmap with a boolean value
     private void updateBoolean(String key, boolean value) {
         updateString(key, String.valueOf(value));
     }
 
+    // update database and hashmap with string value
     private void updateString(String  key, String value) {
+        // update hashmap
         configData.put(key, value);
+        // update database
         preferences.put(key, value);
     }
 
