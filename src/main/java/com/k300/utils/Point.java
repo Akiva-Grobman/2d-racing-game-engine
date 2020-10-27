922b4ee64f458075c06d4017ae20de86937afe6c
akiva <akiva.grobman@gmail.com>
2020-10-27 19:51:47 +0200

    added documentation to Point.java

--- src/main/java/com/k300/utils/Point.java
+++ src/main/java/com/k300/utils/Point.java
@@ -1,14 +1,23 @@
 package com.k300.utils;
 
+/*
+*   Purpose:
+*       store coordinates with double values
+*/
+
 public class Point {
 
+    // contains the x value
     public double x;
+    // contains the y value
     public double y;
 
+    // default initialization with no starting values
     public Point() {
 
     }
 
+    // custom initialization with both values as parameters
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
