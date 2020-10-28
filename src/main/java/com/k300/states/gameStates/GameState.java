24ba46b591ade5e75d9a8e45d2cdf23fe1151654
akiva <akiva.grobman@gmail.com>
2020-10-28 19:09:40 +0200

    added documentation to GameState.java

--- src/main/java/com/k300/states/gameStates/GameState.java
+++ src/main/java/com/k300/states/gameStates/GameState.java
@@ -7,28 +7,43 @@ import com.k300.utils.Point;
 
 import java.awt.*;
 
+/*
+*       Purpose:
+*           this is an abstract representation of a game
+*       Contains:
+*           a track and default starting position
+*/
+
 public abstract class GameState extends State {
 
     public static final Point startingPosition = new Point(800, 880);
+    // this contains the current state of the game (see more in Track, OnlineTrack, OfflineTrack classes)
     protected volatile Track track;
 
+    // only initialization option
     public GameState(Launcher launcher) {
+        // initialize abstract state
         super(launcher);
     }
 
+    // update track
     @Override
     public void tick() {
         track.tick();
     }
 
+    // render track
     @Override
     public void render(Graphics graphics) {
+        // this will make sure we only render the track when it's initialized that might not happen right away
         if(track == null) {
             return;
         }
+        // render track
         track.render(graphics);
     }
 
+    // accessor for the launcher reference
     public Launcher getLauncher() {
         return launcher;
     }
