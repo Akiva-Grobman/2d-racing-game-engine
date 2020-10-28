6a364da72f0d27ffe01d90db118454df9c7afb07
akiva <akiva.grobman@gmail.com>
2020-10-28 22:45:05 +0200

    removed unnecessary MouseListener.java

--- src/main/java/com/k300/io/MouseListener.java
+++ src/main/java/com/k300/io/MouseListener.java
@@ -6,17 +6,31 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
+/*
+*       Purpose:
+*           pass on to a UIManager it the mouse information
+*       Contains:
+*           a UIManager, and current mouse position
+*
+*/
+
 public class MouseListener implements java.awt.event.MouseListener, MouseMotionListener {
 
+    // contains the current mouse position
     private final Point mousePosition;
+    // the current UIManager
     private UIManager uiManager;
 
+    // default initialization to null manager
     public MouseListener() {
         this(null);
     }
 
+    // custom constructor with manager as parameter
     public MouseListener(UIManager uiManager) {
+        // set manager
         this.uiManager = uiManager;
+        // initialize point with not values
         mousePosition = new Point();
     }
 
@@ -30,6 +44,7 @@ public class MouseListener implements java.awt.event.MouseListener, MouseMotionL
 
     }
 
+    // update manager that mouse was released
     @Override
     public void mouseReleased(MouseEvent e) {
         if(uiManager != null) {
@@ -52,15 +67,19 @@ public class MouseListener implements java.awt.event.MouseListener, MouseMotionL
 
     }
 
+    // when the mouse is moved
     @Override
     public void mouseMoved(MouseEvent e) {
+        // update the current mouse position
         mousePosition.x = e.getX();
         mousePosition.y = e.getY();
+        // update the manager that the mouse was moved
         if(uiManager != null) {
             uiManager.onMouseMove(e);
         }
     }
 
+    // manager modifier
     public void setUiManager(UIManager uiManager) {
         this.uiManager = uiManager;
     }
