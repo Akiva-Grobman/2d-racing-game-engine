b7eeb8b1d8e50c58c4a091cb97921f5ea077b75b
akiva <akiva.grobman@gmail.com>
2020-10-28 19:01:14 +0200

    added documentation to MenuState.java

--- src/main/java/com/k300/states/MenuState.java
+++ src/main/java/com/k300/states/MenuState.java
@@ -10,83 +10,108 @@ import com.k300.ui.buttons.*;
 import com.k300.ui.listeners.ClickListener;
 import com.k300.utils.math.Converter;
 
+/*
+*       Purpose:
+*           this represents the menu state, the user will choose to start a game online/offline or enter the settings of exit the program.
+*       Contains:
+*           a UIManager for the buttons and a background
+*/
+
 public class MenuState extends State {
 
+    // this will handle the buttons (update, render, on click)
     private final UIManager uiManager;
+    // this will be displayed in the background
     private final MenuBackground background;
 
     private final double bigButtonWidth;
     private final double bigButtonHeight;
-    private final double xBigButton;
-    private final double startingYBigButton;
-    private final double bigButtonMargin;
-
-
     private final double smallButtonWidth;
     private final double smallButtonHeight;
-    private final double smallButtonMargin;
-
     private final int bigButtonFontSize;
     private final int smallButtonFontSize;
 
-
+    // only initialization option
     public MenuState(Launcher launcher) {
+        // initialize abstract state
         super(launcher);
+        // initialize empty manager
         uiManager = new UIManager();
+        // initialize the background
         background = new MenuBackground();
+        // set manager as mouse listener
         launcher.getMouseListener().setUiManager(uiManager);
-
+        // set big button dimensions
         bigButtonWidth = Converter.FHD_SCREEN_WIDTH / 2.5f;
         bigButtonHeight = Converter.FHD_SCREEN_HEIGHT / 4.5f;
-
-        xBigButton = (Converter.FHD_SCREEN_WIDTH - bigButtonWidth) / 2;
-        startingYBigButton = (Converter.FHD_SCREEN_HEIGHT) / 8.5f;
-        bigButtonMargin = bigButtonHeight * 1.4;
-
-        smallButtonMargin = 40;
+        // an easy way to understand how the buttons are rendered, is to imagine a rectangle and all buttons are set in side this rectangle
+        // set left edge (of our hypothetical rectangle)
+        final double leftEdge = (Converter.FHD_SCREEN_WIDTH - bigButtonWidth) / 2;
+        // set the top edge (of our hypothetical rectangle)
+        final double topEdge = (Converter.FHD_SCREEN_HEIGHT) / 8.5f;
+        // set margins
+        final double bigButtonMargin = bigButtonHeight * 1.4;
+        final double smallButtonMargin = 40;
+        // set small button dimensions
         smallButtonWidth = (bigButtonWidth - smallButtonMargin) / 2f;
         smallButtonHeight = bigButtonHeight / 1.8;
-
+        // set font sizes
         bigButtonFontSize = 100;
         smallButtonFontSize = 40;
-
-        UIMenuButton playButton = getPlayButton(launcher);
-        UIMenuButton onlineButton = getOnlineButton(launcher, playButton);
-        UIMenuButton settingsButton = getSettingsButton(onlineButton);
+        // initialize a button to start a game (offline)
+        UIMenuButton playButton = initBigButton(leftEdge,
+                topEdge,
+                "PLAY",
+                launcher::startOfflineGame);
+        // initialize a button to start an online game
+        UIMenuButton onlineButton = initBigButton(leftEdge,
+                topEdge + bigButtonMargin,
+                "ONLINE",
+                () -> StateManager.setCurrentState(new OnlineState(launcher)));
+        // initialize a button that will change to the settings menu
+        UIMenuButton settingsButton = initSmallButton(leftEdge,
+                topEdge + (2 * bigButtonMargin),
+                "SETTINGS",
+                        () -> StateManager.setCurrentState(new SettingsState(launcher)));
+        // initialize an exit button that will close the program
+        UIMenuButton exitButton = initSmallButton(leftEdge + smallButtonWidth + smallButtonMargin,
+                topEdge + (2 * bigButtonMargin),
+                "EXIT",
+                Launcher::stop);
+        // add the buttons to the manager
         uiManager.addUIObject(playButton);
         uiManager.addUIObject(onlineButton);
         uiManager.addUIObject(settingsButton);
-        uiManager.addUIObject(getExitButton(settingsButton));
+        uiManager.addUIObject(exitButton);
     }
 
-    private UIMenuButton getPlayButton(Launcher launcher) {
-        ClickListener listener = launcher::startOfflineGame;
-        return new UIMenuButton((int) xBigButton, (int) startingYBigButton, (int) bigButtonWidth, (int) bigButtonHeight, "PLAY", bigButtonFontSize, listener);
+    // initialize a big button
+    private UIMenuButton initSmallButton(double x, double y, String text, ClickListener clickListener) {
+        return initButton(x, y, bigButtonWidth, bigButtonHeight, bigButtonFontSize, text, clickListener);
     }
 
-    private UIMenuButton getOnlineButton(Launcher launcher, UIMenuButton playButton) {
-        ClickListener listener = () -> StateManager.setCurrentState(new OnlineState(launcher));
-        return new UIMenuButton((int) xBigButton, (int) (playButton.getY() + bigButtonMargin), (int) bigButtonWidth, (int) bigButtonHeight,"ONLINE", bigButtonFontSize, listener);
+    // initialize a small menu button
+    private UIMenuButton initBigButton(double x, double y, String text, ClickListener clickListener) {
+        return initButton(x, y, smallButtonWidth, smallButtonHeight, smallButtonFontSize, text, clickListener);
     }
 
-    private UIMenuButton getSettingsButton(UIMenuButton multiplayerButton) {
-        ClickListener listener = () -> StateManager.setCurrentState(new SettingsState(launcher));
-        return new UIMenuButton((int) xBigButton, (int) (multiplayerButton.getY() + bigButtonMargin), (int) smallButtonWidth, (int) smallButtonHeight,"SETTINGS", smallButtonFontSize, listener);
+    // initialize a menu button
+    private UIMenuButton initButton(double x, double y, double width, double height, double fontSize, String text, ClickListener clickListener) {
+        return new UIMenuButton((int) x, (int) y, (int) width, (int) height, text, (int) fontSize, clickListener);
     }
 
-    private UIMenuButton getExitButton(UIMenuButton settingsButton) {
-        ClickListener listener = Launcher::stop;
-        return new UIMenuButton((int) (xBigButton + smallButtonWidth + smallButtonMargin), (int) settingsButton.getY(), (int) smallButtonWidth, (int) smallButtonHeight,"EXIT", smallButtonFontSize, listener);
-    }
-
+    // update the background (it contains moving parts)
     @Override
     public void tick() {
         background.tick();
     }
 
+    // render background and buttons
     @Override
     public void render(Graphics graphics) {
+        // render background
         background.render(graphics);
+        // render buttons
         uiManager.render(graphics);
     }
 
