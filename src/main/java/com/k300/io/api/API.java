5f9abc1387c8c813d65d97bcb510e8f42e808fa4
akiva <akiva.grobman@gmail.com>
2020-10-29 12:31:07 +0200

    added documentation to API.java, Room.java, Player.java

--- src/main/java/com/k300/io/api/API.java
+++ src/main/java/com/k300/io/api/API.java
@@ -9,11 +9,32 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
+/*
+*       Purpose:
+*           this class contains abstract representations of the methods that will interact with the server.
+*       Methods:
+*           (get) initLocalCar(sumOfPlayers)
+*               this will send a get request to a url that is determined by the method parameter.
+*               the url looks as follows: base url + /api/quick-match/ + the method parameter (a number representing the sum of players the game will have).
+*               the server will respond to this by adding the local player to a room (if all rooms are full a new one will be opened).
+*               the program will wait for the response asynchronously,
+*               the servers response will contain a GameStartingInfo object that contains all the necessary data to start a game(see more in the GameStartingInfo class).
+*           (post) updateCarData(roomId, postBody)
+*               this will set a post request to a url that is determined by the methods first parameter.
+*               the url looks as follows: base url + /api/room/ + the first parameter of the method (the room id).
+*               the post body will contain a PostBody object that contains all of the local players info (see more in the PostBody class).
+*               the server will update the info of the player based on the post request body.
+*               the program will wait for the response asynchronously,
+*               the servers response will contain a PostResponse object that contains the data of the other players (see more in the PostBody class).
+*/
+
 public interface API {
 
+    // this get request will add the local player to a room and get the local players starting info.
     @GET("/api/quick-match/{sumOfPlayers}")
-    Call<GameStartingInfo> getCarLocalColor(@Path("sumOfPlayers")  int sumOfPlayers);
+    Call<GameStartingInfo> initLocalCar(@Path("sumOfPlayers")  int sumOfPlayers);
 
+    // this post request will update the server representation of the local player and will get back the info of the other players
     @POST("/api/room/{roomId}")
     Call<PostResponse> updateCarData(@Path("roomId") String roomId, @Body PostBody postBody);
 
