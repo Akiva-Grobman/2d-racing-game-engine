10b2bc8537d453f9faaed25ba3bd52d13a7a0f2f
akiva <akiva.grobman@gmail.com>
2020-11-08 17:03:23 +0200

    added documentation to InitialGetHandler.java and PostRequestHandler.java

--- src/main/java/com/k300/io/api/handlers/InitialGetHandler.java
+++ src/main/java/com/k300/io/api/handlers/InitialGetHandler.java
@@ -10,27 +10,35 @@ import retrofit2.Response;
 *           this will handle the first get request response.
 *           the response will contain a GameStartingInfo object (see more GameStartingInfo class).
 *           the room id and player in the web interactor will be set by the response object(GameStartingInfo).
+*
 */
 
 public class InitialGetHandler extends MyCallBack<GameStartingInfo> {
 
-    private volatile GameStartingInfo startingInfo;
-
+    // only initialization option
     public InitialGetHandler(WebInteractor webInteractor) {
+        // initialize abstract call back
         super(webInteractor);
-        startingInfo = null;
     }
 
+    // this method will be called when the server sends a response
     @Override
     public void onResponse(Call<GameStartingInfo> call, Response<GameStartingInfo> response) {
+        // call the parent method for on response
         super.onResponse(call, response);
-        startingInfo = response.body();
+        // initialize a GameStartingInfo object from the response object
+        GameStartingInfo startingInfo = response.body();
+        // set the player field in the webInteractor
         webInteractor.setLocalPlayer(startingInfo.getPlayer());
+        // set the room id field in the webInteraction
         webInteractor.setRoomId(startingInfo.getRoom().getId());
+        // if in dev mode
         if(isInDevMode) {
+            // print the response object
             System.out.print("Initial Get Request response\n>>\n");
             System.out.println(WebInteractor.getPrettyGson().toJson(startingInfo));
         }
+        // this method call will finish the setup of the online game
         webInteractor.finishGameStateSetup();
     }
 
--- src/main/java/com/k300/io/api/handlers/PostRequestHandler.java
+++ src/main/java/com/k300/io/api/handlers/PostRequestHandler.java
@@ -5,24 +5,40 @@ import com.k300.io.api.models.PostResponse;
 import retrofit2.Call;
 import retrofit2.Response;
 
+/*
+*       Purpose:
+*           this will handle all post request responses.
+*           the response body will contain a PostResponse object (see more in the PostResponse class).
+*           the updateCars method from the webInteractor object will be called with the updated data form the server.
+*/
+
 public class PostRequestHandler extends MyCallBack<PostResponse> {
 
+    // only initialization option
     public PostRequestHandler(WebInteractor webInteractor) {
+        // initialize abstract call back
         super(webInteractor);
     }
 
+    // this method will be called when the server sends a response to a post request
     @Override
     public void onResponse(Call<PostResponse> call, Response<PostResponse> response) {
+        // call the parent method for on response
         super.onResponse(call, response);
+        // store the response body
         final PostResponse body = response.body();
+        // if in dev mode
         if(isInDevMode) {
+            // print response
             System.out.print("Update Response \n>>\n");
             System.out.println(WebInteractor.getPrettyGson().toJson(body));
         }
+        // update the game status field in the webInteractor object (true/false)
         webInteractor.setGameIsStarted(body.getRoom().isGameStarted());
         if(isInDevMode) {
             System.out.println("Updating cars...");
         }
+        // this will tell the webInteractor object to update the cars based on the updated info
         webInteractor.updateCars(body.getPlayers());
     }
 
