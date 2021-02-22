10dc735b0f6c7643478b99f2184c04ddfcfdaaeb
ELK4N4 <elkana.hendler@gmail.com>
2021-02-22 21:07:04 +0200

    styled url changer dialog

--- src/main/java/com/k300/display/ServerUrlWindow.java
+++ src/main/java/com/k300/display/ServerUrlWindow.java
@@ -1,37 +1,64 @@
 package com.k300.display;
 
 import com.k300.Launcher;
+import com.k300.graphics.Assets;
+import com.k300.graphics.FontLoader;
 import com.k300.states.OnlineState;
 import com.k300.states.StateManager;
 import com.k300.utils.configarations.Config;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+
+import static com.k300.utils.Utils.drawStringInCenter;
+import static com.k300.utils.Utils.resizeImage;
 
 public class ServerUrlWindow extends JDialog implements ActionListener {
     static JTextField serverUrlBox;
     Launcher launcher;
 
     public ServerUrlWindow(Launcher launcher) {
+        Font minecraft = FontLoader.loadFont("Minecraft", 40);
+        Font boxFont = new Font("Courier New", Font.BOLD, 38);
         JComponent caller = launcher.getWindowJComponent();
         this.launcher = launcher;
         this.setUndecorated(true);
         this.setAlwaysOnTop(true);
-        this.setLayout(new GridBagLayout());
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
-        panel.setBackground(new Color(160, 160, 160));
-        JLabel toastLabel = new JLabel("Enter server IP:");
-        serverUrlBox = new JTextField(16);
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 450, 100, 450));
+        panel.setBackground(new Color(38, 2, 2));
+        panel.setLayout(new GridLayout(3,1,0,60));
+
+        JLabel windowLabel = new JLabel("ENTER SERVER URL :");
+        windowLabel.setFont(minecraft);
+        windowLabel.setForeground(Color.white);
+
+        serverUrlBox = new JTextField(20);
+        serverUrlBox.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        serverUrlBox.setFont(boxFont);
         serverUrlBox.setText(Config.getUrl());
-        JButton confirmBtn = new JButton("Confirm");
+
+        BufferedImage buttonImage = Assets.getImage(Assets.BUTTON_KEY);
+        buttonImage = resizeImage(buttonImage, 500, 150);
+        Graphics buttonGraphics = buttonImage.getGraphics();
+        buttonGraphics.setFont(minecraft);
+        drawStringInCenter(0, 0, buttonImage.getWidth(), buttonImage.getHeight(), buttonGraphics, "CONFIRM");
+        JButton confirmBtn = new JButton(new ImageIcon(buttonImage));
+        confirmBtn.setSize(10,10);
+        confirmBtn.setBorder(BorderFactory.createEmptyBorder());
+        confirmBtn.setContentAreaFilled(false);
         confirmBtn.addActionListener(this);
-        toastLabel.setForeground(Color.WHITE);
-        panel.add(toastLabel);
+
+        panel.add(windowLabel);
         panel.add(serverUrlBox);
         panel.add(confirmBtn);
         this.add(panel);
@@ -39,7 +66,7 @@ public class ServerUrlWindow extends JDialog implements ActionListener {
 
         java.awt.Window window = SwingUtilities.getWindowAncestor(caller);
         int xCoordinate = window.getLocationOnScreen().x + window.getWidth() / 2 - this.getWidth() / 2;
-        int yCoordinate = window.getLocationOnScreen().y + (int)((double)window.getHeight() * 0.75) - this.getHeight() / 2;
+        int yCoordinate = window.getLocationOnScreen().y + window.getHeight() / 2 - this.getHeight() / 2;
         this.setLocation(xCoordinate, yCoordinate);
         this.setShape(new RoundRectangle2D.Double(0, 0, this.getWidth(), this.getHeight(), 30, 30));
         this.setVisible(true);
